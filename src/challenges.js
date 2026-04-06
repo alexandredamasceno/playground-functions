bcffc07c280e629c81eeecabe19317f36851fb14
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2026-04-05 22:27:40 -0300

    Faz os desafios de 1 a 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,27 +1,44 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(valueOne, valueTwo) {
   // seu código aqui
-}
+  return valueOne === true && valueTwo === true;
+};
+//console.log(compareTrue(false, false));
 
 // Desafio 2
-function calcArea() {
+function calcArea(width, height) {
   // seu código aqui
+  return (width * height) / 2;
 }
+//console.log(calcArea(5, 2));
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(phrase) {
   // seu código aqui
+  let charArray = phrase.split(" ");
+  return charArray;
 }
+//console.log(splitSentence("vamo que vamo"));
+
 
 // Desafio 4
-function concatName() {
+function concatName(array) {
   // seu código aqui
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
+//let exempleArray = ['captain', 'my', 'captain'];
+//console.log(concatName(exempleArray));
+
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let victorys = wins * 3;
+  let allTies = ties * 1;
+  return victorys + allTies;
 }
+console.log(footballPoints(14, 8));
+
 
 // Desafio 6
 function highestCount() {
